c9e209c6c60aa3232db778a1e2d3041e9e57ce70
Brainiac Rawkib <brainiacrawkib@gmail.com>
2022-08-09 08:19:15 +0100

    added flask_wtf

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-from datetime import datetime
-
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
@@ -10,6 +11,11 @@ app.config["SECRET_KEY"] = "RANDOM STRING TO GUESS"
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 @app.errorhandler(404)
@@ -22,11 +28,8 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    name = None
+    form = ''
     return render_template('index.html', current_time=datetime.utcnow())
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
